e14a70e938c56cf225fee8970cde22d4ef3aada2
ozakiryota <kb22056@gmail.com>
2020-07-08 01:45:15 +0900

    updated

--- run.sh
+++ run.sh
@@ -9,4 +9,5 @@ nvidia-docker run -it --rm \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--net=host \
+	-v /home/amsl/Documents/AirSim/settings.json:/root/Documents/AirSim/settings.json \
 	$image_name:latest
